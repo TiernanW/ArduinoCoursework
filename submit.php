91b0c0caee8563ab2fe9a4c7ccbf5c13a7ae256b
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 19:41:14 +0000

    Styled table

--- submit.php
+++ submit.php
@@ -1,20 +1,50 @@
-<?php
-	try {
-		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<!DOCTYPE html>
 
-		// For inserting scores via post
-		if(isset($_POST['name']) && isset($_POST['score'])) {
-			$conn->query("INSERT INTO Plays (Username, Score) VALUES ('".$_POST['name']."', ".$_POST['score'].")");
+<html>
+
+<head>
+	<title>Run n Jump - Leaderboard</title>
+	<style type="text/css">
+		.wrapper {
+			width: 960px;
+			margin: 0 auto;
 		}
-		
-		// Displays all scores
-		foreach($conn->query("SELECT * FROM Plays") as $row) {
-			echo "Username: " . $row[1] . " Score: " . $row[2] . "\n";
+		.profile {
+			display: block;
+			width: 100%;
+			margin: 10px 0;
 		}
-	}
-	catch (PDOException $e) {
-		print("Error connecting to SQL Server.");
-		die(print_r($e));
-	}
-?>
+	</style>
+</head>
+
+<body>
+	<div class="wrapper">
+		<header>
+			<h1>Leaderboard</h1>
+		</header>
+		<main>
+		<?php
+			try {
+				$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				// For inserting scores via post
+				if(isset($_POST['name']) && isset($_POST['score'])) {
+					$conn->query("INSERT INTO Plays (Username, Score) VALUES ('".$_POST['name']."', ".$_POST['score'].")");
+				}
+				
+				// Displays all scores
+				foreach($conn->query("SELECT TOP 20 * FROM Plays") as $row) {
+					echo "<div class=\"profile\"><strong>" . $row[1] . "</strong> - " . $row[2] . "</div>";
+				}
+			}
+			catch (PDOException $e) {
+				print("Error connecting to SQL Server.");
+				die(print_r($e));
+			}
+		?>
+		</main>
+	</div>
+</body>
+
+</html>
