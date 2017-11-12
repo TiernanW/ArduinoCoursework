d277ce26cbca72dc0b309e726b1b32c892a17965
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 16:49:54 +0000

    syntax error

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 	try {
 		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		foreach($conn->query("SELECT * FROM Plays") as $row)) {
+		foreach($conn->query("SELECT * FROM Plays") as $row) {
 			echo "a result";
 		}
 	}
