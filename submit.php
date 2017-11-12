ec48d0540e49a925f86e6c9cd45da621248bb628
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 17:48:29 +0000

    Insert Score Test

--- submit.php
+++ submit.php
@@ -2,6 +2,11 @@
 	try {
 		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		if(isset($_POST['name']) && isset($_POST['score'])) {
+			$conn->query("INSERT INTO Plays (Name, Score) VALUES ('".$_POST['name']."', ".$_POST['score'].")");
+		}
+		
 		foreach($conn->query("SELECT * FROM Plays") as $row) {
 			echo "Username: " . $row[1] . " Score: " . $row[2] . "\n";
 		}
@@ -9,10 +14,5 @@
 	catch (PDOException $e) {
 		print("Error connecting to SQL Server.");
 		die(print_r($e));
-	}
-	
-	if(isset($_POST['name']) && isset($_POST['score'])) {
-		echo $_POST['name'];
-		echo $_POST['score'];
 	}
 ?>
