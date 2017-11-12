75d47249cea6e2a107655171b81cdd5e27cdaf94
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 17:57:54 +0000

    Fixed Column Name

--- submit.php
+++ submit.php
@@ -3,10 +3,12 @@
 		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
+		// For inserting scores via post
 		if(isset($_POST['name']) && isset($_POST['score'])) {
-			$conn->query("INSERT INTO Plays (Name, Score) VALUES (\"".$_POST['name']."\", ".$_POST['score'].")");
+			$conn->query("INSERT INTO Plays (Username, Score) VALUES (\"".$_POST['name']."\", ".$_POST['score'].")");
 		}
 		
+		// Displays all scores
 		foreach($conn->query("SELECT * FROM Plays") as $row) {
 			echo "Username: " . $row[1] . " Score: " . $row[2] . "\n";
 		}
