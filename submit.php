04008c6c26e3261bc1331f1b2b81990e9c4f2339
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 16:51:16 +0000

    Actual results

--- submit.php
+++ submit.php
@@ -3,7 +3,7 @@
 		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		foreach($conn->query("SELECT * FROM Plays") as $row) {
-			echo "a result";
+			echo "Username: " + $row[1] + " Score: " + $row[2] + "\n";
 		}
 	}
 	catch (PDOException $e) {
