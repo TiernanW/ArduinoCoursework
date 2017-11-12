631b8f6a26e811610b1404b540573e004ac4428c
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 16:43:52 +0000

    sdsdf

--- submit.php
+++ submit.php
@@ -1,29 +1,11 @@
-<?php/*
-$con=mysqli_init(); 
-mysqli_real_connect($con, "myardui.mysql.database.azure.com", "tiernan@myardui", "T1ernan09", "mysql", 3306);
-$sql = "SELECT * FROM Plays";
+<!DOCTYPE html>
 
-if($result = mysqli_query($con, $sql)) {
-	if (mysqli_num_rows($result) > 0) {
-		echo "sdfs";
-		while($row = mysqli_fetch_assoc($result)) {
-			echo "id: " . $row["Id"]. " - Name: " . $row["Name"]. " - Score: " . $row["Score"]. "<br>";
-		}
-	} else {
-		echo "0 results";
-	}
-} 
+<head>
+	<title>dfgdfg</title>
+</head>
 
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-} */
+<body>
+	fdgfgd
+</body>
 
-echo "hi";
-
-?>
+</html>
