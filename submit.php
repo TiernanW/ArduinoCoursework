e7123300993e4a8dd70364f1a780a163ac61df2e
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-12 16:45:31 +0000

    Added DB connect

--- submit.php
+++ submit.php
@@ -1,3 +1,10 @@
 <?php
-	echo "Hello world!";
+	try {
+		$conn = new PDO("sqlsrv:server = tcp:tw-ardui.database.windows.net,1433; Database = ardui", "tiernan", "T1ernan09");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+		print("Error connecting to SQL Server.");
+		die(print_r($e));
+	}
 ?>
